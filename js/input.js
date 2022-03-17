1e468657d59fb9b95de42b5ad5f4c14f1788fec4
Clement Azalbert <clementazalbert04@gmail.com>
2022-03-17 12:29:20 +0100

    added input.js file + started adding functions in it

---
+++ js/input.js
@@ -0,0 +1,24 @@
+let inputDirection = { x: 0, y: 0};
+let lastInputDirection = { x: 0, y: 0 };
+
+window.addEventListener('keydown', event => {
+  switch (event.key) {
+    case 'ArrowUp':
+      inputDirection = { x: 0, y: -1 }
+      break
+    case 'ArrowDown':
+      inputDirection = { x: 0, y: 1 }
+      break
+    case 'ArrowLeft':
+      inputDirection = { x: -1, y: 0 }
+      break
+    case 'ArrowRight':
+      inputDirection = { x: 1, y: 0 }
+      break
+  }
+})
+
+export function getInputDirection() {
+  lastInputDirection = inputDirection;
+  return inputDirection;
+}
