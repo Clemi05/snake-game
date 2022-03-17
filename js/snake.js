2e503e0f82d660c34ab40e7fca0ce9a61fd35c1a
Clement Azalbert <clementazalbert04@gmail.com>
2022-03-17 12:10:40 +0100

    updated snake css + function update()

--- js/snake.js
+++ js/snake.js
@@ -1,15 +1,22 @@
 export const SNAKE_SPEED = 1;
-const snakeBody = [{ x: 11, y: 11 }];
+const snakeBody = [
+  { x: 10, y: 11 }
+];
 
 export function update() {
-  console.log('update snake');
+  for (let index = snakeBody.length - 2 ; index >= 0; index--){
+    snakeBody[index + 1] = {...snakeBody[i]};
+  }
+
+  snakeBody[0].x += 1;
+  snakeBody[0].y += 0;
 }
 
 export function draw(gameBoard) {
   snakeBody.forEach(segment => {
     const snakeElement = document.createElement('div');
-    snakeElement.style.gridRowStart = segment.x;
-    snakeElement.style.gridColumnStart = segment.y;
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
     snakeElement.classList.add('snake');
     gameBoard.appendChild(snakeElement);
   });
